ff5b326c527ca1a47b255f73f52baa98c22fd26b
venkatroshan <35727735+venkatroshan@users.noreply.github.com>
2018-03-02 01:18:29 -0800

    Create kl.c

---
+++ kl.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char string[50],i,c=0;
+printf("enter a characters");
+gets(strings);
+for(i=0;str[i]!='\0';i++)
+{
+if(strings[i]>'a' &&str[i]<'z')
+{
+c++;
+}
+}
+printf("%d",c);
+}
