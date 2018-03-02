c9574df325c534d6822c898aa9c06abe18e2648a
venkatroshan <35727735+venkatroshan@users.noreply.github.com>
2018-03-02 01:00:17 -0800

    Create hg.c

---
+++ hg.c
@@ -0,0 +1,20 @@
+include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+if(n>0)
+{
+printf("The no is positive");
+}
+else if(n<0)
+{
+printf("The no is negative");
+}
+else
+{
+printf("The no is equal to 0");
+}
+return 0;
+}
