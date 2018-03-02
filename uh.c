ffbfbe6969edf452d0cc91f3671b7292865e7246
venkatroshan <35727735+venkatroshan@users.noreply.github.com>
2018-03-02 00:56:32 -0800

    Create uh.c

---
+++ uh.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int  a,b,c ;
+printf("enter time to start :");
+scanf("%d",&a);
+printf("enter the time end ");
+scanf("%d",&b);
+
+c=a-b;
+printf("time in hrs %d");
+return 0;
+}
